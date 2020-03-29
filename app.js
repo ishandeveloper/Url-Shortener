b1d9731ea3b44dc79d0a5d4ceb910b2295951a70
Ishan Sharma <ishandeveloper@outlook.com>
2020-03-29 23:50:36 +0530

    Finalize

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+var port=process.env.PORT || 8080;
+
+//Setting Template Engine
+app.set('view engine', 'ejs');
+
+//Serving Static Files
+app.use('/assets', express.static('assets'));
+app.use(bodyParser.json());
+app.listen(port,()=>{
+  console.log("Server Up  → PORT "+port);
+});
+
+require("./routes/main")(app);
