80b48f9ae1dd71add8a89a239e9e742ee7b17da5
Ishan Sharma <ishandeveloper@outlook.com>
2020-03-29 14:34:01 +0530

    dependencies

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express=require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+require('./models/url');
 
 //Creating Express Server
 const app=express();
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('Listening at : '+port);
+
+
+//Connecting To MongoDB
+const connectOptions = {
+    keepAlive: true,
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+  };
+
+  mongoose.Promise = global.Promise;
+  mongoose.connect(process.env.DATABASE, connectOptions, (err, db) => {
+    if (err) console.log(`Error`, er);
+    console.log(`Connected to MongoDB`);
+  });
